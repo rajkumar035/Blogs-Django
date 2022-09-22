58085725d6b8326b9ea1f59b9cf2677b77971d3f
Rajkumar D <kishorekumardhandapani71@gmail.com>
2022-09-22 23:01:59 +0530

    Update Post feature implemented

--- task/urls.py
+++ task/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from . import views
-from .views import PostListView, PostDetailView, PostCreateView
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView
 
 urlpatterns = [
     path('', PostListView.as_view(), name='task-first'), #as_view() is for assigning the list to render/perform in the view page format
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-det'), #It was like an dynamic url <int:pk> returns the primary key of the post
-    path('post/new/', PostCreateView.as_view(), name='post-create'), #It was like an dynamic url <int:pk> returns the primary key of the post
+    path('post/new/', PostCreateView.as_view(), name='post-create'), 
+    path('post/<int:pk>/edit', PostUpdateView.as_view(), name='post-edit'),
     path('about/', views.about, name='task-about')
 ]
--- task/views.py
+++ task/views.py
@@ -1,10 +1,12 @@
+from operator import truediv
 from django.shortcuts import render
 from .models import Post
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import (
     ListView, #Show all our posts in list view to manage(ordering, rendering, etc...)
     DetailView, #detail view is for showing the post with that same data detailed in another page
-    CreateView ) #It is for
+    CreateView, #It is for creating a new list of data like in the prototype
+    UpdateView ) #It is for updating/editing the post
 
 def first(request):
     context = {
@@ -20,12 +22,25 @@ class PostListView(ListView):
 class PostDetailView(DetailView):
     model = Post
 
-class PostCreateView(LoginRequiredMixin, CreateView): # LoginRequiredMixin sets our application to only create post when user logged in
+class PostCreateView(LoginRequiredMixin, CreateView): # LoginRequiredMixin sets our application to only create post when user logged in and another argument called create view is for passing that this data should be managed by this method
     model = Post
     fields = ['name', 'password', 'dob']
     def form_valid(self, form):
         form.instance.client = self.request.user
         return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView): # UserPassesTestMixin it is for that we saying that that the client of the post should only update and another argument called update view is for updating the data of the selected post
+    model = Post
+    fields = ['name', 'password', 'dob']
+    def form_valid(self, form):
+        form.instance.client = self.request.user
+        return super().form_valid(form)
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.client:
+            return True
+        else:
+            return False
 
 def about(request):
     return render(request, 'task/about.html', {'title': 'About'})
