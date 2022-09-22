6644af16602dd43378528f67f10bfec84525a5d0
Rajkumar D <kishorekumardhandapani71@gmail.com>
2022-09-23 00:21:02 +0530

    Added Delete feature and complete blog app with crud feature

--- task/urls.py
+++ task/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 from . import views
-from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
 
 urlpatterns = [
-    path('', PostListView.as_view(), name='task-first'), #as_view() is for assigning the list to render/perform in the view page format
-    path('post/<int:pk>/', PostDetailView.as_view(), name='post-det'), #It was like an dynamic url <int:pk> returns the primary key of the post
-    path('post/new/', PostCreateView.as_view(), name='post-create'), 
-    path('post/<int:pk>/edit', PostUpdateView.as_view(), name='post-edit'),
+    # as_view() is for assigning the list to render/perform in the view page format
+    path('', PostListView.as_view(), name='task-first'),
+    # It was like an dynamic url <int:pk> returns the primary key of the post
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-det'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/edit/', PostUpdateView.as_view(), name='post-edit'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
     path('about/', views.about, name='task-about')
 ]
--- task/views.py
+++ task/views.py
@@ -3,38 +3,66 @@ from django.shortcuts import render
 from .models import Post
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import (
-    ListView, #Show all our posts in list view to manage(ordering, rendering, etc...)
-    DetailView, #detail view is for showing the post with that same data detailed in another page
-    CreateView, #It is for creating a new list of data like in the prototype
-    UpdateView ) #It is for updating/editing the post
+    # Show all our posts in list view to manage(ordering, rendering, etc...)
+    ListView,
+    DetailView,  # detail view is for showing the post with that same data detailed in another page
+    CreateView,  # It is for creating a new list of data like in the prototype
+    UpdateView,  # It is for updating/editing the post
+    DeleteView)  # It deletes the data from the list
+
 
 def first(request):
     context = {
         'posts': Post.objects.all()
     }
-    return render(request, 'task/home.html', context) #3rd attribute is for sending some data it acts like an props
+    # 3rd attribute is for sending some data it acts like an props
+    return render(request, 'task/home.html', context)
+
 
 class PostListView(ListView):
-    model = Post #Assigning the Post(from model) to view in list view
-    template_name = 'task/home.html' #For assigning the data should be displayed in this template to view
-    context_object_name = 'posts' #Sending the object of datas using this line
+    model = Post  # Assigning the Post(from model) to view in list view
+    # For assigning the data should be displayed in this template to view
+    template_name = 'task/home.html'
+    context_object_name = 'posts'  # Sending the object of datas using this line
+
 
 class PostDetailView(DetailView):
     model = Post
 
-class PostCreateView(LoginRequiredMixin, CreateView): # LoginRequiredMixin sets our application to only create post when user logged in and another argument called create view is for passing that this data should be managed by this method
+# Creating Post
+# LoginRequiredMixin sets our application to only create post when user logged in and another argument called create view is for passing that this data should be managed by this method
+class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
     fields = ['name', 'password', 'dob']
+
     def form_valid(self, form):
         form.instance.client = self.request.user
         return super().form_valid(form)
 
-class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView): # UserPassesTestMixin it is for that we saying that that the client of the post should only update and another argument called update view is for updating the data of the selected post
+# Updating Post
+# UserPassesTestMixin it is for that we saying that that the client of the post should only update and another argument called update view is for updating the data of the selected post
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
     fields = ['name', 'password', 'dob']
+
     def form_valid(self, form):
         form.instance.client = self.request.user
         return super().form_valid(form)
+
+    def test_func(self):  # This function is for saying that the user/client of the post only should update the post
+        post = self.get_object()
+        if self.request.user == post.client:
+            return True
+        else:
+            return False
+
+# Deleting Post
+# deleteview is for passing that this data should be deleted by this method
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    # It redirect after deleting the post to the metioned path
+    success_url = "/"
+
     def test_func(self):
         post = self.get_object()
         if self.request.user == post.client:
@@ -42,5 +70,6 @@ class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView): # Use
         else:
             return False
 
+# About Page
 def about(request):
     return render(request, 'task/about.html', {'title': 'About'})
